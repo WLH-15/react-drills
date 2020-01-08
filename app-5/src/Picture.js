3eb3d06d4b5b0cffc54a91eed2b4231174f64f83
Todd Davis <toddjohndavis@gmail.com>
2020-01-08 14:43:34 -0700

    done

---
+++ app-5/src/Picture.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+class Picture extends React.Component{
+    render(){
+        return(
+            <div>
+                <img src={this.props.url}/>
+                <p1> Error 559</p1>
+            </div>
+        )
+    }
+}
+export default Picture
