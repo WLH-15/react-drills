d53141b810b9e89462163a37bc53cec033698840
Todd Davis <toddjohndavis@gmail.com>
2020-01-08 15:46:24 -0700

    done

--- app-6/src/App.js
+++ app-6/src/App.js
@@ -1,26 +1,49 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Todo from './Todo';
 
-function App() {
-  return (
+class App extends React.Component {
+  constructor(){
+    super()
+    this.state={
+      array: [],
+      input: ''
+    }
+    // this.addedMate = this.addedMate.bind(this)
+  }
+  changling(val){
+    this.setState({input: val})
+  }
+  addedMate =() =>{
+    this.setState({
+      array: [...this.state.array, this.state.input], 
+      input: ''
+    });}
+
+  render(){
+    // console.log(this.sta1te.array)
+    let array = this.state.array.map((element, index)=>{
+      return <Todo key={index} task={element} />
+          })
+    return(
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>My to-do list:</h1>
+
+      <div>
+      <input 
+      value={this.state.input}
+      onChange={e=> this.changling(e.target.value)}
+      placeholder='Enter New Task' />
+      <button onClick={this.addedMate} >Add</button>
+      
+      </div>
+
+      <br />
+      {array}
     </div>
   );
 }
+}
+
 
 export default App;
